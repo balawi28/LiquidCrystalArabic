befebd972bdace2a68a39e3cb02c48b3840901a4
Balawi <41299807+balawi28@users.noreply.github.com>
2025-01-25 12:05:05 +0100

    Update LiquidCrystalArabic.h
    remove duplicate "public:"

--- LiquidCrystalArabic.h
+++ LiquidCrystalArabic.h
@@ -664,8 +664,6 @@ private:
             return true;
         return false;
     }
-
-    public: 
     
 public:
     LiquidCrystalArabic(uint8_t rs, uint8_t enable, uint8_t d0, uint8_t d1, uint8_t d2, uint8_t d3): LiquidCrystal(rs, enable, d0, d1, d2, d3),
